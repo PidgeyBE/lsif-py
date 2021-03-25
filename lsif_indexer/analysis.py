4709d264e419a8e0843f882390716fcc7319eb48
Pieterjan <pieterjan.soetaert@robovision.eu>
2021-03-25 14:57:57 +0100

    fix for python 0.18.0

--- lsif_indexer/analysis.py
+++ lsif_indexer/analysis.py
@@ -22,7 +22,7 @@ class Name:
         will only include one object. If this name is a definition,
         the list will include itself.
         """
-        return [Name(a) for a in self.definition.goto_assignments() if a != self.definition]
+        return [Name(a) for a in self.definition.goto() if a != self.definition]
 
     @property
     def line(self):
@@ -51,11 +51,10 @@ def get_names(source, filename):
     """
     Retrieve a list of Name objects for the given source.
     """
-    definitions = jedi.names(
-        source,
-        path=filename,
+
+    definitions = jedi.Script(source, path=filename).get_names(
         all_scopes=True,
+        definitions=True,
         references=True,
     )
-
     return [Name(d) for d in definitions]
